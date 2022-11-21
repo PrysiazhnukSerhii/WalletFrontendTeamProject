ccdd1ead06f833458c311fd421e0b8eb7d83cf7b
PrysiazhnukSerhii <feremiks@gmail.com>
2022-11-21 17:25:57 +0100

    made  header for token

--- src/redux/authSlice.jsx
+++ src/redux/authSlice.jsx
@@ -4,15 +4,15 @@ export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://wallet-backend-team-project.herokuapp.com',
-    // prepareHeaders: (headers, { getState }) => {
-    //   const token = getState().userInformation.token;
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().userInformation.token;
 
-    //   if (token) {
-    //     headers.set('authorization', `Bearer ${token}`);
-    //   }
+      if (token) {
+        headers.set('authorization', `${token}`);
+      }
 
-    //   return headers;
-    // },
+      return headers;
+    },
   }),
   endpoints: builder => ({
     signUpUser: builder.mutation({
