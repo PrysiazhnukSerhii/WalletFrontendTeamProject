e7a0faba554d9a85d112c6ade3a7ee59b6f3d013
TetianaKolochko <92599848+TetianaKolochko@users.noreply.github.com>
2022-11-25 23:56:41 +0200

    n

--- src/pages/dashboardPage/DashboardPage.styled.jsx
+++ src/pages/dashboardPage/DashboardPage.styled.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 // import ellipseDesktopUp from '../../images/bg-desktop-login/ellipse-up-desktop.png';
 import ellipseDown from '../../images/bg-tablet-login/ellipse-down.png';
 import ellipseUp from '../../images/bg-tablet-login/ellipse-up.png';
+import ellipseDownX from '../../images/bg-tablet-login/ellipse-down@2x.png';
+import ellipseUpX from '../../images/bg-tablet-login/ellipse-up@2x.png';
+import ellipseDownDesktop from '../../images/bg-desktop-login/ellipse.png';
+import ellipseUpDesktop from '../../images/bg-desktop-login/ellipse-up-desktop.png';
 
 export const Section = styled.section``;
 
@@ -26,45 +30,37 @@ export const ContainerDashboard = styled.div`
 `;
 
 export const MainPage = styled.main`
-  
-    position: relative;
-    background-color: var(--secondary-background-color);
-    width: 100%;
-    height: 568px;
+background-color: var(--secondary-background-color);
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    height: 1024px;
+    
+    background-repeat: no-repeat;
 
-    @media screen and (min-width: 768px) {
-      width: 1280px;
-      height: 720px;
-      
-      background-image: url(${ellipseDown}),
-        url(${ellipseUp});
-        
+    background-image: url(${ellipseDown}), url(${ellipseUp});
 
-      @media screen and (min-device-pixel-ratio: 2),
-        screen and (min-resolution: 192dpi),
-        screen and (min-resolution: 2dppx) {
-        .main {
-          background-image: url(../../images/bg-tablet-login/ellipse-down@2x.png),
-            url(../../images/bg-tablet-login/ellipse-up@2x.png),
-            url(../../images/frame-login/frame-tablet@2x.png);
-        }
+    @media screen and (min-device-pixel-ratio: 2),
+      screen and (min-resolution: 192dpi),
+      screen and (min-resolution: 2dppx) {
+      .main {
+        background-image: url(${ellipseDownX}), url(${ellipseUpX});
       }
       background-repeat: no-repeat;
       background-position: bottom 0 left 0, top 0 right 0, top 60px left 114px;
     }
 
-    @media screen and (min-width: $DesktopWidth) {
+    @media screen and (min-width: 1280px) {
       width: 1280px;
       height: 720px;
-      background-image: url(../../images/frame-login/frame-desctop.png),
-        url(../../images/bg-desktop-login/ellipse.png),
-        url(../../images/bg-desktop-login/rectangle.png);
+     
+        url(${ellipseDownDesktop}),
+        url(${ellipseUpDesktop});
 
       @media screen and (min-device-pixel-ratio: 2),
         screen and (min-resolution: 192dpi),
         screen and (min-resolution: 2dppx) {
         .main {
-          background-image: url(../../images/frame-login/frame-desctop@2x.png),
+          
             url(../../images/bg-desktop-login/ellipse@2x.png),
             url(../../images/bg-desktop-login/rectangle@2x.png);
         }
