157cbebe3bcb9ba739fe409eb7246b6d8e69bdf9
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-11-28 22:45:28 +0200

    currency Rates 3

--- src/pages/dashboardPage/dashboardPage.jsx
+++ src/pages/dashboardPage/dashboardPage.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import { Currency } from '../../components/currency/Currency';
+
 import Media from 'react-media';
 import {
   Section,
@@ -9,13 +10,38 @@ import {
   MainPage,
 } from './dashboardPage.styled';
 import { Dashboard } from '../../components/dashboard/dashboard';
-
+// -----------------------
 export function DashboardPage() {
   return (
     <>
       <Header />
       <Currency />
-      <Outlet />
+      <MainPage>
+        <Media
+          query="(max-width: 767px)"
+          render={() => (
+            <Container>
+              <ContainerDashboard>
+                <Dashboard />
+              </ContainerDashboard>
+              <Outlet />
+            </Container>
+          )}
+        />
+        <Media
+          query="(min-width: 768px)"
+          render={() => (
+            <Section>
+              <Container>
+                <ContainerDashboard>
+                  <Dashboard />
+                  <Outlet />
+                </ContainerDashboard>
+              </Container>
+            </Section>
+          )}
+        />
+      </MainPage>
     </>
   );
 }
