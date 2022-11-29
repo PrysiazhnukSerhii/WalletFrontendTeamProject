393ce51deacd228e2aaa525d880c55bd937fda79
PrysiazhnukSerhii <feremiks@gmail.com>
2022-11-29 08:53:00 +0100

    fix/homeTab

--- src/pages/dashboardPage/dashboardPage.jsx
+++ src/pages/dashboardPage/dashboardPage.jsx
@@ -1,4 +1,4 @@
-import { Header } from '../../components/Header/Header';
+import { Header } from '../../components/headerFix/headerFix';
 import { Outlet } from 'react-router-dom';
 
 import Media from 'react-media';
