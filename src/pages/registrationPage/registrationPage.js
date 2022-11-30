68cd8fbecdb10f352fa0ad78d6fedb2b1b519ce7
TetianaKolochko <92599848+TetianaKolochko@users.noreply.github.com>
2022-11-30 11:41:10 +0200

    add

--- src/pages/registrationPage/registrationPage.js
+++ src/pages/registrationPage/registrationPage.js
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import RegistrationForm from '../../components/registrationForm';
-// import '../registrationPage/registerPage.scss';
-// import { ContainerWrapper, PageText } from './register.styled';
+import React from 'react';
+import RegistrationForm from '../../components/registrationForm';
+import '../registrationPage/registerPage.scss';
+import { ContainerWrapper, PageText } from './register.styled';
 
-// const RegistrationPage = () => {
-//   return (
-//     <div>
-//       <ContainerWrapper>
-//         <PageText>Finance App</PageText>
-//         <RegistrationForm />
-//       </ContainerWrapper>
-//     </div>
-//   );
-// };
+const RegistrationPage = () => {
+  return (
+    <div>
+      <ContainerWrapper>
+        <PageText>Finance App</PageText>
+        <RegistrationForm />
+      </ContainerWrapper>
+    </div>
+  );
+};
 
-// export default RegistrationPage;
+export default RegistrationPage;
